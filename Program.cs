9478d743ca72acaf893669b5fbd9eb8138bc81aa
Cristian Bonifacio <Cristian.Bonifacio23@qmail.cuny.edu>
2019-06-19 14:43:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+
+public class Triangle
+{
+    public static bool IsTriangle(int a, int b, int c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0)
+        {
+            return false;
+        }
+
+        if (a + b > c && a + c > b && b + c > a) {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class TriangleTests
+{
+    public void IsTriangle_ValidPostiveNumbers_ReturnsTrue()
+    {
+        Assert.IsTrue(Triangle.IsTriangle(5, 7, 10));
+    }
+
+}
